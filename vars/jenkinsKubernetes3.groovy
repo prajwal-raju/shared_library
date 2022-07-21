2fa44e4b2fdaba96465b0612f6e50a8fd0ea8345
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-07-21 18:07:31 +0530

    Update jenkinsKubernetes3.groovy

--- vars/jenkinsKubernetes3.groovy
+++ vars/jenkinsKubernetes3.groovy
@@ -10,39 +10,38 @@ environment {
 		gitCredId = "${gitcred}"
 	}
 		
-	agent none
+    agent none
 
     stages {
         stage("POLL SCM"){
-			      agent{label 'docker'}
-            steps {
-                git branch: 'gitBranch', credentialsId: ['gitCredID'], url: 'gitRepo'             
-            }
+		agent{label 'docker'}
+            	steps {
+                	git branch: 'gitBranch', credentialsId: ['gitCredID'], url: 'gitRepo'             
+            	}
         } 
         stage('BUILD IMAGE') {
-			      agent{label 'docker'}
-            steps {
-                sh 'docker build -t $registry:$dockerTag_$BUILD_NUMBER .'             
-            }
+		agent{label 'docker'}
+            	steps {
+                	sh 'docker build -t $registry:$dockerTag .'             
+            	}
         }
         stage('PUSH HUB') { 
-			      agent{label 'docker'}
-            steps {
-                script {
-                    docker.withRegistry( '', '$registryCredential' ) {
-                        sh 'docker tag $registry:$dockerTag_$BUILD_NUMBER $registry:$dockerTag_$BUILD_NUMBER'
-                        sh 'docker push $registry:$dockerTag_$BUILD_NUMBER'
-                    }
-                }    
-            }
+		agent{label 'docker'}
+            	steps {
+                	script {
+                    		docker.withRegistry( '', '$registryCredential' ) {
+                        	sh 'docker push $registry:$dockerTag'
+                    		}
+                	}    
+            	}
         }
         stage('DEPLOY IMAGE') {
-			      agent{label 'kubernetes'}
-			      steps {
-				      git branch: '$gitBranch', credentialsId: ['$gitCredID'], url: '$gitRepo'
-				      sh 'kubectl apply -f manifest.yml --record'
-			      }
-		    }  
+		agent{label 'kubernetes'}
+		steps {
+			git branch: '$gitBranch', credentialsId: ['$gitCredID'], url: '$gitRepo'
+			sh 'kubectl apply -f manifest.yml --record'
+		}
+	}  
     }
 }  
 }

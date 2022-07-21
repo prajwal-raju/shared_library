c746aa0e372fbea70d0088ab3ca3e0f345b3f73a
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-07-21 17:56:41 +0530

    Create jenkinsKubernetes3.groovy

---
+++ vars/jenkinsKubernetes3.groovy
@@ -0,0 +1,48 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent none
+
+    stages {
+        stage("POLL SCM"){
+			      agent{label 'docker'}
+            steps {
+                git branch: 'gitBranch', credentialsId: ['gitCredID'], url: 'gitRepo'             
+            }
+        } 
+        stage('BUILD IMAGE') {
+			      agent{label 'docker'}
+            steps {
+                sh 'docker build -t $registry:$dockerTag_$BUILD_NUMBER .'             
+            }
+        }
+        stage('PUSH HUB') { 
+			      agent{label 'docker'}
+            steps {
+                script {
+                    docker.withRegistry( '', '$registryCredential' ) {
+                        sh 'docker tag $registry:$dockerTag_$BUILD_NUMBER $registry:$dockerTag_$BUILD_NUMBER'
+                        sh 'docker push $registry:$dockerTag_$BUILD_NUMBER'
+                    }
+                }    
+            }
+        }
+        stage('DEPLOY IMAGE') {
+			      agent{label 'kubernetes'}
+			      steps {
+				      git branch: '$gitBranch', credentialsId: ['$gitCredID'], url: '$gitRepo'
+				      sh 'kubectl apply -f manifest.yml --record'
+			      }
+		    }  
+    }
+}  
+}

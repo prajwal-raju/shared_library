c5c848e67ef66c17f0c7e44cb40825da04f22920
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-07-22 16:45:12 +0530

    Update jenkinsKubernetes5.groovy

--- vars/jenkinsKubernetes5.groovy
+++ vars/jenkinsKubernetes5.groovy
@@ -10,45 +10,33 @@ environment {
 		gitCredId = "${gitcred}"
 	}
 		
-	agent none
-	
-	stages {
-		stage("POLL SCM"){
-      agent{label 'docker'}
-			steps {
-				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
-			}
-		}	
-					
-		stage('BUILD IMAGE') {
-       agent{label 'docker'}
-			 steps { 
-				 script { 
-					 dockerimage = dockerImage = docker.build registry + ":$dockerTag" 
-				 }
-			} 
-		}
-					
-		stage('PUSH HUB') { 
-       agent{label 'docker'}
-			 steps { 
-				 script {
-					 docker.withRegistry( '', registryCredential ) { 
-			                        dockerImage.push() 
-                    			}
-                		}		
-			} 
-		}
-					
-		stage('DEPLOY IMAGE') {
-      agent{label 'eks'}
-			steps {
-			  checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
-			  sh 'kubectl apply -f manifest.yml --record'
-		}
-		}
-	}
-			  
-}
+    agent none
 
+    stages {
+        stage("POLL SCM"){
+		agent{label 'docker'}
+            	steps {
+                	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
+            	}
+        } 
+        stage('BUILD IMAGE') {
+		agent{label 'docker'}
+            	steps {
+                	sh 'docker build -t $registry:$dockerTag .'             
+            	}
+        }
+        stage('PUSH HUB') { 
+		agent{label 'docker'}
+            	steps {
+			sh 'docker push $registry:$dockerTag'                   	
+                }    
+        }
+        stage('DEPLOY IMAGE') {
+		agent{label 'eks'}
+		steps {
+			sh 'kubectl set image deploy webapp-deployment nodejs="$registry:$dockerTag" --record'
+		}
+	}  
+    }
+}  
 }

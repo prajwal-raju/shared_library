e23346e30ac140daca10d4270033a0382d1bd485
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-08-18 07:04:15 +0530

    Update kubernetesShellTest.groovy

--- vars/kubernetesShellTest.groovy
+++ vars/kubernetesShellTest.groovy
@@ -1,4 +1,4 @@
-def call(String registryCred = 'a', String registryname = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a', string depname = 'a', string contname = 'a') {
+def call(String registryCred = 'a', String registryname = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a', String depname = 'a', String contname = 'a') {
 
 pipeline {
 environment { 

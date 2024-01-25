bb93008cffe8ee76db00b7de0eab0f53d0505f73
prajwal-raju <131363609+prajwal-raju@users.noreply.github.com>
2024-01-25 15:23:52 +0530

    Update sharedlibrarytest.groovy added the docker agent

--- vars/sharedlibrarytest.groovy
+++ vars/sharedlibrarytest.groovy
@@ -10,25 +10,29 @@ environment {
 		gitCredId = "${gitcred}"
 	}
 		
-    agent any
+    agent none
 
     stages {
         stage("POLL SCM"){
+		agent {label 'docker'}
             	steps {
                 	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
             	}
         } 
         stage('BUILD IMAGE') {
+		agent {label 'docker'}
             	steps {
                 	sh 'docker build -t $registry:$dockerTag .'             
             	}
         }
         stage('PUSH HUB') { 
+		agent {label 'docker'}
             	steps {
 			sh 'docker push $registry:$dockerTag'                   	
                 }    
         }
         stage('DEPLOY IMAGE') {
+		agent {label 'docker'}
 		steps {
 			sh 'docker run -itd -p 3080:3080 --name "$registry-$dockerTag" "$registry:$dockerTag"'
 		}
